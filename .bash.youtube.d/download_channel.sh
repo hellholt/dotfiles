0eb21f4ce72f428adc5dafe5d1e73ffb2869acdf
Nathan Douglas <github@darkdell.net>
2022-01-16 13:07:31 -0500

    YouTube Poop.

---
+++ .bash.youtube.d/download_channel.sh
@@ -0,0 +1,11 @@
+ #!/usr/bin/env bash
+
+# Download a channel.
+yt_download_channel() {
+  : "${2?"Usage: ${FUNCNAME} URL BASE_PATH"}";
+  the_url="${1}";
+  the_base_path="${2}";
+  youtube-dl "${the_url}" \
+    --output "${the_base_path}/%(uploader)s/%(release_date)s %(title)s.%(ext)s";
+}
+
---
+++ .bash.youtube.d/download_new.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Download new videos from specified sources.
+yt_download_new() {
+  : "${1?"Usage: ${FUNCNAME} BASE_PATH"}";
+  the_base_path="${1}";  
+  awk '!/^ *#/ && NF' ~/.config/youtube-dl/channels.txt \
+    | while read the_url; do
+      yt_download_channel "${the_url}" "${the_base_path}";
+    done
+}
