76db385a731374b42a1bcc69a893df556227acae
Nathan Douglas <github@darkdell.net>
2022-01-31 18:04:03 -0500

    Nevermind.

--- .bash.youtube.d/download_channel.sh
+++ .bash.youtube.d/download_channel.sh
@@ -6,7 +6,6 @@ yt_download_channel() {
   the_url="${1}";
   the_base_path="${2}";
   yt-dlp "${the_url}" \
-    --paths "temp:/tmp" \
     --output "${the_base_path}/%(uploader)s/%(upload_date)s %(title)s.%(ext)s";
 }
 
