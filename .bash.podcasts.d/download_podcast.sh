675b31c981ff9e367d6e91946dea7d53f1dd5e7a
Nathan Douglas <github@darkdell.net>
2022-02-04 11:23:00 -0500

    Adds pod_download_podcast function.

---
+++ .bash.podcasts.d/download_podcast.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Download a podcast.
+pod_download_podcast() {
+  : "${2?"Usage: ${FUNCNAME} URL ROOT_DIRECTORY"}";
+  the_url="${1}";
+  root_directory="${2}";
+  podcast-dl \
+    --out-dir "${root_directory}/{{podcast_title}}" \
+    --archive "${root_directory}/{{podcast_title}}/archive.json" \
+    --episode-template "{{release_date}} {{title}}" \
+    --include-meta \
+    --include-episode-meta \
+    --url "${the_url}";
+}
+
