4c6afea5ef1821583eb9928d1984899f636ca698
Nathan Douglas <github@darkdell.net>
2022-02-04 12:38:31 -0500

    Adds pod_download_new function.

---
+++ .bash.podcasts.d/download_new.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Download new podcasts.
+pod_download_new() {
+  : "${1?"Usage: ${FUNCNAME} ROOT_DIRECTORY"}";
+  root_directory="${1}";
+  awk '!/^ *#/ && NF' ~/.config/podcast-dl/podcasts.txt \
+    | while read the_url; do
+      echo "Downloading from ${the_url}";
+      pod_download_podcast "${the_url}" "${root_directory}";
+    done
+}
