7ed59daca305642a6f80b5b817b6a14a359e7512
Nathan Douglas <github@darkdell.net>
2022-02-18 13:00:16 -0500

    Sort sources randomly.

--- .bash.podcasts.d/download_new.sh
+++ .bash.podcasts.d/download_new.sh
@@ -4,7 +4,7 @@
 pod_download_new() {
   : "${1?"Usage: ${FUNCNAME} ROOT_DIRECTORY"}";
   root_directory="${1}";
-  awk '!/^ *#/ && NF' ~/.config/podcast-dl/podcasts.txt \
+  awk '!/^ *#/ && NF' ~/.config/podcast-dl/podcasts.txt | sort -R \
     | while read the_url; do
       echo "Downloading from ${the_url}";
       pod_download_podcast "${the_url}" "${root_directory}";
