3190e91a9e79220d54e7026df58ef3e49caff15d
Nathan Douglas <github@darkdell.net>
2022-05-15 10:44:11 -0400

    Move extensions file.

--- .config/code-server/install_extensions.sh
+++ .config/code-server/install_extensions.sh
@@ -3,4 +3,4 @@
 PATH=$PATH:/app/code-server/bin:/usr/local/bin;
 while read extension; do
   code-server --install-extension "${extension}";
-done < "${HOME}/.config/code-server/data/extensions.txt"
+done < "${HOME}/.config/code-server/extensions.txt"
