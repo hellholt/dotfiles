c2687534e06447bd3281b478a4ac7d7b2d33cfbd
Nathan Douglas <github@darkdell.net>
2022-05-16 16:43:30 -0400

    Make yt-dlp more service-agnostic.

--- .bash.youtube.d/download_channel.sh
+++ .bash.youtube.d/download_channel.sh
@@ -6,6 +6,8 @@ yt_download_channel() {
   the_url="${1}";
   the_base_path="${2}";
   yt-dlp "${the_url}" \
+    --download-archive "/volume1/Video/Youtube/downloaded.txt" \
+    --cookies "~/.config/youtube-dl/cookies.txt" \
     --output "${the_base_path}/%(uploader)s/%(upload_date)s %(title)s.%(ext)s";
 }
 
