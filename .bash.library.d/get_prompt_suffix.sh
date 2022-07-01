a8cb16ff674cb22ff9c754a25eac9fd84bbd712a
Nathan Douglas <github@darkdell.net>
2022-07-01 10:07:09 -0400

    Remove some outdated prompt stuff.

--- .bash.library.d/get_prompt_suffix.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# The suffix, styled appropriately.
-nd_get_prompt_suffix() {
-  echo "$(printf "\[$(tput sgr0)$(tput bold)\]\$\[$(tput sgr0)\] ")";
-}
--- .bash.library.d/set_motd.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Set the MOTD.
-nd_set_motd() {
-  nd_get_motd | sudo tee /etc/motd;
-}
-
