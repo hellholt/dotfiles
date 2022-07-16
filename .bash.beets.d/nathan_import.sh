1315d3fa37173cbf7e3607b12637975ba2be78f8
Nathan Douglas <github@darkdell.net>
2022-07-16 19:45:38 -0400

    Skip questions!

--- .bash.beets.d/nathan_import.sh
+++ .bash.beets.d/nathan_import.sh
@@ -12,17 +12,19 @@ beets_nathan_import() {
         find "${the_artist_path}" -mindepth 1 -maxdepth 1 -type d -iname "*${album_expression}*" -print \
           | sort -h \
           | while read the_album_path; do
-              the_artist_name="$(basename "${the_artist_path}")";
-              the_album_name="$(basename "${the_album_path}")";
-              read -p "Import ${the_artist_name}: ${the_album_name}? " -n 1 -r reply < /dev/tty;
-              echo;
               if [ -d "${the_album_path/\/All\//\/Nathan\/}" ]; then
                 echo "Skipping ${the_album_path} because it is already present at the destination...";
-              elif [[ "${reply}" =~ ^[Yy]$ ]]; then
-                echo "Importing ${the_album_path} ....";
-                beets_nathan import "${the_album_path}" < /dev/tty;
-              else
-                echo "Skipping ${the_album_path} ....";
+              else 
+                the_artist_name="$(basename "${the_artist_path}")";
+                the_album_name="$(basename "${the_album_path}")";
+                read -p "Import ${the_artist_name}: ${the_album_name}? " -n 1 -r reply < /dev/tty;
+                echo;
+                if [[ "${reply}" =~ ^[Yy]$ ]]; then
+                  echo "Importing ${the_album_path} ....";
+                  beets_nathan import "${the_album_path}" < /dev/tty;
+                else
+                  echo "Skipping ${the_album_path} ....";
+                fi;
               fi;
             done;
       done;
