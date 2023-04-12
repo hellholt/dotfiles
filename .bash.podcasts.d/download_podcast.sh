f9f591c3ed615becef9bacabd221679bd6bd8795
Nathan Douglas <github@darkdell.net>
2023-04-12 15:36:24 -0400

    `npx podcast-dl`

--- .bash.podcasts.d/download_podcast.sh
+++ .bash.podcasts.d/download_podcast.sh
@@ -5,7 +5,7 @@ pod_download_podcast() {
   : "${2?"Usage: ${FUNCNAME} URL ROOT_DIRECTORY"}";
   the_url="${1}";
   root_directory="${2}";
-  podcast-dl \
+  npx podcast-dl \
     --out-dir "${root_directory}/{{podcast_title}}" \
     --archive "${root_directory}/{{podcast_title}}/archive.json" \
     --episode-template "{{release_date}} {{title}}" \
