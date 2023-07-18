bef5ceb258e410ee716d416cf536c2fa184c6c6c
Nathan Douglas <github@darkdell.net>
2023-07-18 19:49:47 -0400

    That didn't work...

--- .bash.beets.d/nathan_import_queue.sh
+++ .bash.beets.d/nathan_import_queue.sh
@@ -5,15 +5,10 @@ beets_nathan_import_queue() {
   music_root="${MUSIC_ROOT:-/Music}";
   path_file="${music_root}/import_queue.txt";
   cat "${path_file}" | while read the_album_path || [[ -n "${the_album_path}" ]]; do
-    read -p "Import ${the_album_path}? (y/n) " response;
-    if [[ $response =~ ^[Yy]$ ]]; then
-      if beets_nathan import "$album_path" < /dev/tty; then
-        sed -i "/^$album_path$/d" "${path_file}";
-      else
-        echo "Failed to import $album_path" >&2
-      fi
+    if beets_nathan import "$album_path" < /dev/tty; then
+      sed -i "/^$album_path$/d" "${path_file}";
     else
-      echo "Skipping ${the_album_path}";
+      echo "Failed to import $album_path" >&2
     fi
     sleep 1;
   done;
