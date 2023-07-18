6ff0f8fde279d2f0434bb4744ea7fc5de13f8165
Nathan Douglas <github@darkdell.net>
2023-07-18 17:56:45 -0400

    Prune playlists.

--- .bash.beets.d/list_artist.sh
+++ .bash.beets.d/list_artist.sh
@@ -5,7 +5,15 @@ beets_list_artist() {
   : "${1?"Usage: ${FUNCNAME[0]} ARTIST_EXPRESSION"}";
   music_root="${MUSIC_ROOT:-/Music}";
   artist_expression="${@}";
-  find "${music_root}" -mindepth 4 -maxdepth 4 -type d -iname "*${artist_expression}*" -print \
+  find "${music_root}" \
+    -mindepth 4 \
+    -maxdepth 4 \
+    -type d \
+    -iname "*${artist_expression}*" \
+    -not \(\
+      -path "*@eaDir*" \
+    \)\
+    -print \
     | sort -h \
     | while read the_artist_path; do
         basename "${the_artist_path}";
