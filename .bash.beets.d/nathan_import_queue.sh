50ff9c494f6199aecbc2c4ae6ba7fccca13aba6c
Nathan Douglas <github@darkdell.net>
2023-07-18 19:52:16 -0400

    Tweak.

--- .bash.beets.d/nathan_import_queue.sh
+++ .bash.beets.d/nathan_import_queue.sh
@@ -5,8 +5,9 @@ beets_nathan_import_queue() {
   music_root="${MUSIC_ROOT:-/Music}";
   path_file="${music_root}/import_queue.txt";
   cat "${path_file}" | while read the_album_path || [[ -n "${the_album_path}" ]]; do
-    if beets_nathan import "$album_path" < /dev/tty; then
-      sed -i "/^$album_path$/d" "${path_file}";
+    echo "Importing ${the_album_path} ....";
+    if beets_nathan import "${the_album_path}" < /dev/tty; then
+      sed -i "/^${the_album_path}$/d" "${path_file}";
     else
       echo "Failed to import $album_path" >&2
     fi
