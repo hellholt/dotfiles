b4f763ba54fdddc0a4783ca8051c6c87d5550e49
Nathan Douglas <github@darkdell.net>
2024-12-01 17:15:17 -0500

    Update download_podcast().

--- .bash.podcasts.d/download_podcast.sh
+++ .bash.podcasts.d/download_podcast.sh
@@ -8,9 +8,12 @@ pod_download_podcast() {
   npx podcast-dl \
     --out-dir "${root_directory}/{{podcast_title}}" \
     --archive "${root_directory}/{{podcast_title}}/archive.json" \
-    --episode-template "{{release_date}} {{title}}" \
+    --episode-template "{{release_date}} {{episode_num}} {{title}}" \
+    --threads 2 \
     --include-meta \
     --include-episode-meta \
+    --include-episode-transcripts \
+    --add-mp3-metadata \
     --url "${the_url}";
 }
 
