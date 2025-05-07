d30aa28fb3800bba758a9cfdb91f33067a689b42
Nate Douglas <ndouglas@users.noreply.github.com>
2025-05-06 21:31:15 -0400

    Change paths.

--- .bash.youtube.d/download_new.sh
+++ .bash.youtube.d/download_new.sh
@@ -3,8 +3,8 @@
 # Download new videos from specified sources.
 yt_download_new() {
   : "${1?"Usage: ${FUNCNAME} BASE_PATH"}";
-  the_base_path="${1}";  
-  awk '!/^ *#/ && NF' ~/.config/youtube-dl/youtube.*.md | sort -R \
+  the_base_path="${1}";
+  awk '!/^ *#/ && NF' ~/youtube/youtube.*.md | sort -R \
     | while read the_url; do
       yt_download_channel "${the_url}" "${the_base_path}";
     done
