307ce98c52f84ae3dc9dbed28fc790d0a4fcf867
Nate Douglas <ndouglas@users.noreply.github.com>
2025-05-13 17:23:20 -0400

    Pull updates.

--- .bash.podcasts.d/download_new.sh
+++ .bash.podcasts.d/download_new.sh
@@ -4,6 +4,8 @@
 pod_download_new() {
   : "${1?"Usage: ${FUNCNAME} ROOT_DIRECTORY"}";
   root_directory="${1}";
+  cd ~/podcasts;
+  git pull --ff-only;
   awk '!/^ *#/ && NF' ~/podcasts/podcasts.*.md | sort | uniq | sort -R \
     | while read the_url; do
       echo "Downloading from ${the_url}";
--- .bash.youtube.d/download_new.sh
+++ .bash.youtube.d/download_new.sh
@@ -4,6 +4,8 @@
 yt_download_new() {
   : "${1?"Usage: ${FUNCNAME} BASE_PATH"}";
   the_base_path="${1}";
+  cd ~/youtube;
+  git pull --ff-only;
   awk '!/^ *#/ && NF' ~/youtube/youtube.*.md | sort -R \
     | while read the_url; do
       yt_download_channel "${the_url}" "${the_base_path}";
